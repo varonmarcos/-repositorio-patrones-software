cd579a0555bfccc3d48d31b73c3234baac705355
pabloarturot@gmail.com <pabloarturot@gmail.com@a17c92c2-baf4-804e-b514-9c95c907513d>
2013-10-17 03:08:59 +0000

    Organizacion general de los proyectos

---
+++ v1.0.0/BUS-KallsonnysOMS-Business/src/org/kallsonnys/oms/services/OrdersBean.java
@@ -0,0 +1,8 @@
+package org.kallsonnys.oms.services;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class OrdersBean implements OrdersLocal, OrdersRemote {
+
+}
---
+++ v1.0.0/ENT-KallsonnysOMS-Dto/src/org/kallsonys/oms/dto/OrderDTO.java
@@ -0,0 +1,7 @@
+package org.kallsonys.oms.dto;
+
+public class OrderDTO extends BaseDTO{
+
+	private static final long serialVersionUID = 1L;
+
+}
