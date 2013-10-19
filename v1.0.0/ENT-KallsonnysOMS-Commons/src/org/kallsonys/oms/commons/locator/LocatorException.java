4424b21188ee0c33d62e3d7847c315a78293be29
pabloarturot@gmail.com <pabloarturot@gmail.com@a17c92c2-baf4-804e-b514-9c95c907513d>
2013-10-19 04:47:33 +0000

    Localizador de EJB y de Colas

---
+++ v1.0.0/ENT-KallsonnysOMS-Commons/src/org/kallsonys/oms/commons/locator/LocatorException.java
@@ -0,0 +1,29 @@
+package org.kallsonys.oms.commons.locator;
+
+public class LocatorException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public LocatorException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LocatorException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LocatorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LocatorException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+}
