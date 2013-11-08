317e30395e200048d93e51db7799a17bd56eab0f
varonmarcos@gmail.com <varonmarcos@gmail.com@a17c92c2-baf4-804e-b514-9c95c907513d>
2013-11-08 15:58:11 +0000

    version correcta del template del sitio

--- trunk/v1.0.0/NAV-KallsonnysOMS-Web/src/org/kallsonnys/oms/web/beans/LoginBean.java
+++ trunk/v1.0.0/NAV-KallsonnysOMS-Web/src/org/kallsonnys/oms/web/beans/LoginBean.java
@@ -1,12 +1,20 @@
 package org.kallsonnys.oms.web.beans;
 
 import java.io.Serializable;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+
+import org.kallsonnys.oms.dto.UserDTO;
+import org.kallsonnys.oms.dto.security.IntialUserLoginDTO;
+import org.kallsonnys.oms.utilities.Util;
+import org.kallsonys.oms.commons.Exception.OMSException;
 import org.primefaces.context.RequestContext;
+
+import test.DAO;
 
 @ManagedBean(name = "loginBean")
 @ViewScoped
@@ -15,7 +23,7 @@ public class LoginBean implements Serializable {
 	private String inputUserName;
 	private String inputPassword;
 	private boolean logeado = false;
-
+	
 	public String getInputPassword() {
 		return inputPassword;
 	}
@@ -39,19 +47,46 @@ public class LoginBean implements Serializable {
 	public void login(ActionEvent actionEvent) {
 		RequestContext context = RequestContext.getCurrentInstance();
 		FacesMessage msg = null;
-		if (this.inputUserName != null && this.inputUserName.equals("admin") && this.inputPassword != null && this.inputPassword.equals("admin")) {
+		IntialUserLoginDTO ini = null;
+		DAO d = new DAO();
+		UserDTO userDto = null;
+		logeado = true;	
+		
+		try {
+			ini = d.getTransaccionesPendientes(inputUserName, inputPassword);
+			userDto = ini.getUser();
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", userDto.getName());
+		} catch (OMSException e) {
+			logeado = false;
+			System.out.println("ERROR login: " + e.getMessage());
+			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Credenciales no válidas");
+			e.printStackTrace();
+		} catch (Exception e) {
+			logeado = false;
+			System.out.println("ERROR login: " + e.getMessage());
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Error", "Error de validación");
+			e.printStackTrace();
+		}
+		
+		
+		
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		
+		context.addCallbackParam("estaLogeado", logeado);
+		
+		if (logeado){			
+			Util.putinSession("userSesion", userDto);
+			context.addCallbackParam("view", "pages/welcome.xhtml"); 
+		}
+		
+		/*if (this.inputUserName != null && this.inputUserName.equals("pabloarturot@gmail.com") && this.inputPassword != null && this.inputPassword.equals("admin")) {
+			validateUserPerfil(this.inputUserName, this.inputPassword);
 			logeado = true;
 			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenid@", this.inputUserName);
 		} else {
 			logeado = false;
 			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Credenciales no válidas");
-		}
-
-		FacesContext.getCurrentInstance().addMessage(null, msg);
-		context.addCallbackParam("estaLogeado", logeado);
-		
-		if (logeado){ context.addCallbackParam("view", "initialMenu.xhtml"); }
-		
+		}*/	
 
 	}
 }
