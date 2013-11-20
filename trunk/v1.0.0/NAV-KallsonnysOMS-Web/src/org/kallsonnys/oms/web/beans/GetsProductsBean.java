26c790c0fc057a53c0646509a4e10283548032ae
varonmarcos@gmail.com <varonmarcos@gmail.com@a17c92c2-baf4-804e-b514-9c95c907513d>
2013-11-20 05:08:53 +0000

    ajustes pequeños de camio en el nombre de la clase

--- trunk/v1.0.0/NAV-KallsonnysOMS-Web/src/org/kallsonnys/oms/web/beans/GetsProductsBean.java
+++ trunk/v1.0.0/NAV-KallsonnysOMS-Web/src/org/kallsonnys/oms/web/beans/GetsProductsBean.java
@@ -8,19 +8,14 @@ import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.event.ActionEvent;
 
-import org.kallsonnys.oms.dto.FilterConstants;
 import org.kallsonnys.oms.dto.ProductDTO;
-import org.kallsonnys.oms.dto.TableFilterDTO;
-import org.kallsonnys.oms.dto.TableResultDTO;
 import org.kallsonnys.oms.enums.ProducerTypeEnum;
 import org.kallsonnys.oms.enums.ProductCategoryEnum;
 import org.kallsonnys.oms.utilities.Util;
-import org.primefaces.context.RequestContext;
+import org.kallsonnys.oms.web.beans.model.ProductDTOLazyList;
 import org.primefaces.event.RowEditEvent;
-
-import test.DAO;
+import org.primefaces.model.LazyDataModel;
 
 @ManagedBean(name = "getsProducts")
 @ViewScoped
@@ -28,12 +23,9 @@ public class GetsProductsBean implements Serializable {
 	
 	private static final long serialVersionUID = -2152389656664659476L;
 	
-	private Integer criterio;
-	private String inputFindValue;	
-	
 	private ProductDTO producto;
-	private List<ProductDTO> productos;
-	private int totalOfRecords; 
+	private LazyDataModel<ProductDTO> productos;
+	private List<ProductDTO> list;
 	private List<ProductCategoryEnum> categorys;
 	private List<ProducerTypeEnum> producers;
 	
@@ -43,35 +35,7 @@ public class GetsProductsBean implements Serializable {
 	
 	
 	public GetsProductsBean(){
-		
-	}
-	
-	public void find(ActionEvent actionEvent){
-		RequestContext context = RequestContext.getCurrentInstance(); 
-		DAO d = new DAO();
-		TableResultDTO<ProductDTO> result = null;		
-		TableFilterDTO filterDTO = new  TableFilterDTO();
-		
-		filterDTO.setStart(0);
-		filterDTO.setPageSize(20);
-		filterDTO.setSorterType(TableFilterDTO.ASC);
-		filterDTO.addFilter(Util.mapCriterioFilter(criterio), inputFindValue);		
-		
-		result = d.getProducts(filterDTO);
-		productos = result.getResult();
-		totalOfRecords = result.getTotalOfRecords();	
-		
-		if (totalOfRecords > 0){
-			messageHeader = "Registros Encontrados ";
-			messageBody = "";
-			severity = FacesMessage.SEVERITY_INFO;
-		}else{
-			messageHeader = "No se Encontraron Registros ";
-			messageBody = criterio.toString() + " input " + inputFindValue;
-			severity = FacesMessage.SEVERITY_WARN;
-		}
-
-		Util.addMessage(severity, messageHeader, messageBody);
+		setProductos(new ProductDTOLazyList(list));
 	}
 	
 	public void onEdit(RowEditEvent event) {  
@@ -83,45 +47,12 @@ public class GetsProductsBean implements Serializable {
     }  
       
     public void onCancel(RowEditEvent event) {  
-        FacesMessage msg = new FacesMessage("Producto Eliminado", ((ProductDTO) event.getObject()).getProdId().toString()); 
         messageHeader = "Producto Eliminado";
 		messageBody = ((ProductDTO) event.getObject()).getProdId().toString();
 		severity = FacesMessage.SEVERITY_INFO;
 		
 		Util.addMessage(severity, messageHeader, messageBody);  
     } 	
-
-	public Integer getCriterio() {
-		return criterio;
-	}
-
-	public void setCriterio(Integer criterio) {
-		this.criterio = criterio;
-	}
-
-	public String getInputFindValue() {
-		return inputFindValue;
-	}
-
-	public void setInputFindValue(String inputFindValue) {
-		this.inputFindValue = inputFindValue;
-	}
-
-	public List<ProductDTO> getProductos() {
-		return productos;
-	}
-
-	public void setProductos(List<ProductDTO> productos) {
-		this.productos = productos;
-	}
-
-	public int getTotalOfRecords() {
-		return totalOfRecords;
-	}
-
-	public void setTotalOfRecords(int totalOfRecords) {
-		this.totalOfRecords = totalOfRecords;
-	}
 
 	public List<ProducerTypeEnum> getProducers() {
 		producers = new ArrayList<ProducerTypeEnum>();
@@ -153,6 +84,14 @@ public class GetsProductsBean implements Serializable {
 
 	public void setProducto(ProductDTO producto) {
 		this.producto = producto;
+	}
+
+	public LazyDataModel<ProductDTO> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(LazyDataModel<ProductDTO> productos) {
+		this.productos = productos;
 	}
 	
 	
