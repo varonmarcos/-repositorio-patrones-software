1efacda7cdd63c5437dd6a681fb5fc74e0639ea6
varonmarcos@gmail.com <varonmarcos@gmail.com@a17c92c2-baf4-804e-b514-9c95c907513d>
2013-11-24 20:57:06 +0000

    consulta de ordenes y actulizacion estado de estado  de ordenes

---
+++ trunk/v1.0.0/NAV-KallsonnysOMS-Web/src/org/kallsonnys/oms/web/beans/UpdateOrderBean.java
@@ -0,0 +1,248 @@
+package org.kallsonnys.oms.web.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.kallsonnys.oms.dto.AddressDTO;
+import org.kallsonnys.oms.dto.CustomerDTO;
+import org.kallsonnys.oms.dto.ItemDTO;
+import org.kallsonnys.oms.dto.OrderDTO;
+import org.kallsonnys.oms.dto.ProductDTO;
+import org.kallsonnys.oms.enums.AddressTypeEnum;
+import org.kallsonnys.oms.enums.CountryEnum;
+import org.kallsonnys.oms.enums.CustomerStatusEnum;
+import org.kallsonnys.oms.enums.OrderStatusEnum;
+import org.kallsonnys.oms.enums.ProducerTypeEnum;
+import org.kallsonnys.oms.utilities.Util;
+
+import test.DAO;
+
+@ManagedBean(name = "updateOrder")
+@ViewScoped
+public class UpdateOrderBean implements Serializable {
+
+	private static final long serialVersionUID = -2152389656664659476L;
+
+	private Long inputId;
+	private Date inputDate;
+	private Double inputPrice;
+	private OrderStatusEnum inputState;
+	private String inputComments;
+	private String slCustomer;
+	private String slShipper;
+	private ProducerTypeEnum slProduccer;
+	private OrderDTO orden;
+	private List<ItemDTO> items;
+	
+	private boolean disCotizar;
+	private boolean disApprove;
+	private boolean disFactory;
+	private boolean disSend;
+	
+	private String cotizaSony;
+	private String cotizaRapidService;
+
+	public UpdateOrderBean() {
+		HttpServletRequest req = (HttpServletRequest) FacesContext
+				.getCurrentInstance().getExternalContext().getRequest();
+
+		if (req != null) {
+			String idOrd = req.getParameter("id");
+			if (idOrd != null) {
+
+				System.out.println("id " + idOrd);
+				DAO d = new DAO();
+				
+				//invocacion EJB
+				// orden = d.editCliente();
+
+				inputId = orden.getId();
+				inputDate = orden.getOrderDate();
+				inputPrice = orden.getPrice();
+				inputState = orden.getStatus();
+				inputComments = orden.getComments();
+				slCustomer = orden.getCustomer().getName();
+				slShipper = orden.getShippingProvider();
+				slProduccer = orden.getProducer();
+			}
+
+		}
+		
+		if (inputState.equals(OrderStatusEnum.CREATED)){
+			setDisApprove(true);
+		}
+		
+		if (inputState.equals(OrderStatusEnum.APPROVED)){
+			setDisFactory(true);
+			setDisCotizar(true);
+		}
+		
+		if (inputState.equals(OrderStatusEnum.PROCESSING)){
+			setDisSend(true);
+		}
+		
+		
+	}
+	
+	public void approve(){
+		
+	}
+	
+	public void factory(){
+			
+	}
+	
+	public void send(){
+		
+	}
+	
+	
+	public void cotizar(){
+		String resultCotizaSony = null;
+		String resultCotizaRapid = null;
+		
+		//invocacion ejb
+		setCotizaSony(resultCotizaSony);
+		setCotizaRapidService(resultCotizaRapid);
+		
+	}
+
+	public Long getInputId() {
+		return inputId;
+	}
+
+	public void setInputId(Long inputId) {
+		this.inputId = inputId;
+	}
+
+	public Date getInputDate() {
+		return inputDate;
+	}
+
+	public void setInputDate(Date inputDate) {
+		this.inputDate = inputDate;
+	}
+
+	public Double getInputPrice() {
+		return inputPrice;
+	}
+
+	public void setInputPrice(Double inputPrice) {
+		this.inputPrice = inputPrice;
+	}
+
+	public OrderStatusEnum getInputState() {
+		return inputState;
+	}
+
+	public void setInputState(OrderStatusEnum inputState) {
+		this.inputState = inputState;
+	}
+
+	public String getInputComments() {
+		return inputComments;
+	}
+
+	public void setInputComments(String inputComments) {
+		this.inputComments = inputComments;
+	}
+
+	public String getSlCustomer() {
+		return slCustomer;
+	}
+
+	public void setSlCustomer(String slCustomer) {
+		this.slCustomer = slCustomer;
+	}
+
+	public String getSlShipper() {
+		return slShipper;
+	}
+
+	public void setSlShipper(String slShipper) {
+		this.slShipper = slShipper;
+	}
+
+	public OrderDTO getOrden() {
+		return orden;
+	}
+
+	public void setOrden(OrderDTO orden) {
+		this.orden = orden;
+	}
+
+	public List<ItemDTO> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemDTO> items) {
+		this.items = items;
+	}
+
+	public ProducerTypeEnum getSlProduccer() {
+		return slProduccer;
+	}
+
+	public void setSlProduccer(ProducerTypeEnum slProduccer) {
+		this.slProduccer = slProduccer;
+	}
+
+	public boolean isDisCotizar() {
+		return disCotizar;
+	}
+
+	public void setDisCotizar(boolean disCotizar) {
+		this.disCotizar = disCotizar;
+	}
+
+	public boolean isDisApprove() {
+		return disApprove;
+	}
+
+	public void setDisApprove(boolean disApprove) {
+		this.disApprove = disApprove;
+	}
+
+	public boolean isDisFactory() {
+		return disFactory;
+	}
+
+	public void setDisFactory(boolean disFactory) {
+		this.disFactory = disFactory;
+	}
+
+	public boolean isDisSend() {
+		return disSend;
+	}
+
+	public void setDisSend(boolean disSend) {
+		this.disSend = disSend;
+	}
+
+	public String getCotizaSony() {
+		return cotizaSony;
+	}
+
+	public void setCotizaSony(String cotizaSony) {
+		this.cotizaSony = cotizaSony;
+	}
+
+	public String getCotizaRapidService() {
+		return cotizaRapidService;
+	}
+
+	public void setCotizaRapidService(String cotizaRapidService) {
+		this.cotizaRapidService = cotizaRapidService;
+	}
+	
+	
+
+}
